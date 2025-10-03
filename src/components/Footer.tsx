1ae70dfcda92fc91ca6a0d2da6ded934ac17d3b8
Van Vo <worktugal.com@gmail.com>
2025-10-03 01:37:01 +0100

    Updated Layout.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export const Footer: React.FC = () => {
           <div className="space-y-4">
             <a href="/" className="flex items-center space-x-2 hover:opacity-90 transition-opacity">
               <img
-                src="/worktugal-logo-new.png"
+                src="/worktugal-logo-bg-light-radius-1000-1000.png"
                 alt="Worktugal Logo"
                 className="w-9 h-9 object-contain drop-shadow-lg"
                 width="36"
